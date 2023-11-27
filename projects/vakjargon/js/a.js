ce04b0e25d164b53e000a79b67c722182bff49ff
thop01 <p.thong@rocmondriaan.nl>
2023-11-27 11:57:26 +0100

    android & words

---
+++ projects/vakjargon/js/a.js
@@ -0,0 +1,102 @@
+<li>Algorithm</li>
+        <li>Array</li>
+        <li>Variable</li>
+        <li>Function</li>
+        <li>Object</li>
+        <li>Class</li>
+        <li>Method</li>
+        <li>Property</li>
+        <li>Loop</li>
+        <li>Conditional</li>
+        <li>Recursion</li>
+        <li>String</li>
+        <li>Integer</li>
+        <li>Float</li>
+        <li>Boolean</li>
+        <li>Operator</li>
+        <li>Compiler</li>
+        <li>Interpreter</li>
+        <li>Debugging</li>
+        <li>Variable Scope</li>
+        <li>Global Scope</li>
+        <li>Local Scope</li>
+        <li>Namespace</li>
+        <li>Data Type</li>
+        <li>Comment</li>
+        <li>Exception</li>
+        <li>Exception Handling</li>
+        <li>Immutable</li>
+        <li>Mutable</li>
+        <li>Callback</li>
+        <li>Recursion</li>
+        <li>Binary</li>
+        <li>Hexadecimal</li>
+        <li>Source Code</li>
+        <li>Version Control</li>
+        <li>Git</li>
+        <li>Repository</li>
+        <li>Branch</li>
+        <li>Merge</li>
+        <li>Commit</li>
+        <li>Push</li>
+        <li>Pull</li>
+        <li>IDE (Integrated Development Environment)</li>
+        <li>API (Application Programming Interface)</li>
+        <li>Framework</li>
+        <li>Library</li>
+        <li>Module</li>
+        <li>Package</li>
+        <li>Dependency</li>
+        <li>Debugging</li>
+        <li>Unit Test</li>
+        <li>Integration Test</li>
+        <li>Continuous Integration</li>
+        <li>Agile</li>
+        <li>Waterfall</li>
+        <li>Scrum</li>
+        <li>Kanban</li>
+        <li>Object-Oriented Programming</li>
+        <li>Functional Programming</li>
+        <li>Imperative Programming</li>
+        <li>Declarative Programming</li>
+        <li>API Endpoint</li>
+        <li>RESTful</li>
+        <li>HTTP</li>
+        <li>HTTPS</li>
+        <li>JSON</li>
+        <li>XML</li>
+        <li>Database</li>
+        <li>SQL</li>
+        <li>NoSQL</li>
+        <li>ORM (Object-Relational Mapping)</li>
+        <li>CRUD (Create, Read, Update, Delete)</li>
+        <li>Authentication</li>
+        <li>Authorization</li>
+        <li>Session</li>
+        <li>Cookies</li>
+        <li>Middleware</li>
+        <li>Server</li>
+        <li>Client</li>
+        <li>HTTP Request</li>
+        <li>HTTP Response</li>
+        <li>Framework</li>
+        <li>Library</li>
+        <li>Module</li>
+        <li>Package</li>
+        <li>Dependency</li>
+        <li>Abstraction</li>
+        <li>Encapsulation</li>
+        <li>Inheritance</li>
+        <li>Polymorphism</li>
+        <li>Singleton</li>
+        <li>Design Pattern</li>
+        <li>Refactoring</li>
+        <li>Code Review</li>
+        <li>Documentation</li>
+        <li>Agile</li>
+        <li>Scrum</li>
+        <li>Kanban</li>
+        <li>DevOps</li>
+        <li>Continuous Integration</li>
+        <li>Continuous Delivery</li>
+        <li>Continuous Deployment</li>
